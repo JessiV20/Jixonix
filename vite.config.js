4b0e903fbda119ac29e0c9c0b25d75c5abf4ec8e
jessi <jessivilla657@gmail.com>
2025-12-17 16:30:33 -0600

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '/',
+  base: '/LandingServices/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
